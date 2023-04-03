0884e09be6d22598480208ce8c3c5a58d9df25c7
MSchandorff <111353536+MSchandorff@users.noreply.github.com>
2023-04-03 11:01:49 +0200

    Add files via upload

---
+++ ElasticClientNolek/Library/ElasticClient.cs
@@ -0,0 +1,39 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
+namespace Library
+{
+    public class ElasticClient
+    {
+        private static readonly ElasticClient _instance = new ElasticClient();
+        private ElasticClient()
+        {
+            if (_client == null)
+            {
+                var settings = new ElasticsearchClientSettings(new Uri("https://localhost:9200"));
+
+                _client = new ElasticsearchClient(settings);
+            }
+        }
+
+        public static ElasticClient Instance { get { return _instance; } }
+        public ElasticsearchClient _client { get; set; }
+
+
+        public async Task TestDataIndex()
+        {
+            var data = new TestData
+            {
+                StringProperty = "value123"
+            };
+
+            var Response = await _client
+                .IndexAsync(document, "my-document-index");
+
+            //if (Response.IsValidResponse)
+            //{
+            //    Console.WriteLine($"Index document with ID {indexResponse.Id} succeeded.");
+            //}
+        }
+    }
+}
